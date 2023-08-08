8bacee25bf867c71a85a5df6848db70365c20b4f
Rodrigo Dini <83221997+Rodrik227@users.noreply.github.com>
2023-08-08 13:21:44 -0300

    added english version

--- scripts/EN.js
+++ scripts/EN.js
@@ -1,17 +1,17 @@
 const shows = [
-        {
-        Name: "Ensaio Aberto",
-        Data: "20/06/23",
-        Time: "8:30 PM",
-        LocalS: "heilige Pocket",
-        Street: "Marechal deodoro 6969",
-        Block: "Centro",
-        City: "Santa Cruz do Sul",
-        Value: null,
-        Cents: 50,
-        Link: 'https://youtube.com',
-        Button: 'Buy 👨🏿‍🚒'
-    },
+    //     {
+    //     Name: "Ensaio Aberto",
+    //     Data: "20/06/23",
+    //     Time: "8:30 PM",
+    //     LocalS: "heilige Pocket",
+    //     Street: "Marechal deodoro 6969",
+    //     Block: "Centro",
+    //     City: "Santa Cruz do Sul",
+    //     Value: null,
+    //     Cents: 50,
+    //     Link: 'https://youtube.com',
+    //     Button: 'Buy 👨🏿‍🚒'
+    // },
 ]
 
 
